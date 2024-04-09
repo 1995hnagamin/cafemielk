20ae1fa598485da1cba7677bb9cc06c62059f71e
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-04-09 18:16:40 +0900

    Introduce <cm-csr>

--- cafemielk-tight/playground.scm
+++ cafemielk-tight/playground.scm
@@ -1,3 +1,10 @@
 (use cafemielk)
 
 (print "Loading playground file...")
+
+(define-class <cm-csr> ()
+    ((nrows :init-keyword :nrows)
+     (ncols :init-keyword :ncols)
+     (data :init-keyword :data)
+     (indices :init-keyword :indices)
+     (indptr :init-keyword :indptr)))
