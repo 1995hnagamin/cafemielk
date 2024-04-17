6bb00eef53549e67bebc80c07d23be17a03f80b8
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-04-17 22:30:58 +0900

    Update parameter set of `csr-addmv!`

--- cm-tight/cafemielk.scm
+++ cm-tight/cafemielk.scm
@@ -65,12 +65,10 @@
   (make <csr>
     :vals vals :rowptr rowptr :colind colind))
 
-(define (csr-addmv! y A x)
+(define (csr-addmv! nr nc A x y)
   ; y += A*x
-  (define m (vector-length y))
-  (define n (vector-length x))
   (do ((i 0 (+ i 1)))
-      ((= i m) #f)
+      ((= i nr) #f)
     (do ((j (vector-ref (slot-ref A 'rowptr) i) (+ j 1)))
 	((= j (vector-ref (slot-ref A 'rowptr) (+ i 1))) #f)
       (vector-set!
@@ -79,9 +77,9 @@
 	  (* (vector-ref (slot-ref A 'vals) j)
 	     (vector-ref x (vector-ref (slot-ref A 'colind) j))))))))
 
-(define (csr-mv A x)
-  (define y (make-vector (slot-ref A 'nrows) 0))
-  (csr-addmv! y A x)
+(define (csr-mv nr nc A x)
+  (define y (make-vector nr 0))
+  (csr-addmv! nr nc A x y)
   y)
 
 (define-class <coo> ()
