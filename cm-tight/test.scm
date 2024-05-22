f9d419460b8df7d9e178e959e6bbccdb8fd0d70e
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-05-22 11:58:11 +0900

    Add test-mesh2d-square to test.scm

--- cm-tight/test.scm
+++ cm-tight/test.scm
@@ -13,6 +13,11 @@
  #(b c d)
  (vview->vector (make-vview #(a b c d) 1 #(3))))
 
+(test*
+ "test-mesh2d-square"
+ 121
+ (mesh2d-nodes-length (mesh2d-square 11 11)))
+
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
