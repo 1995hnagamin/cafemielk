b6d21b4915311eb98ada0dc5fa844e1c607b682c
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-05-22 11:50:47 +0900

    Move variables to mesh.scm

--- cm-tight/cafemielk.scm
+++ cm-tight/cafemielk.scm
@@ -6,18 +6,11 @@
   (extend
    cafemielk.util
    cafemielk.linalg
+   cafemielk.mesh
    cafemielk.vview)
   (export
-   <mesh2d>
    eval-at-triangle
    func->fel
-   make-mesh2d
-   mesh2d-nodes
-   mesh2d-nodes-length
-   mesh2d-nodes-ref
-   mesh2d-triangles
-   mesh2d-triangles-ref
-   square
    )
   )
 (select-module cafemielk)
@@ -25,45 +18,9 @@
 ;; Loads extension
 (dynamic-load "cafemielk")
 
-;;
-;; Put your Scheme definitions here
-;;
-
-;; cafemielk.mesh
-
+(use cafemielk.mesh)
 (use cafemielk.util)
 (use cafemielk.vview)
-
-(define-class <mesh2d> ()
-  ((nodes :init-keyword :nodes)
-   (triangles :init-keyword :triangles)))
-
-(define (make-mesh2d nodes triangles)
-  (make <mesh2d> :nodes nodes :triangles triangles))
-
-(define (mesh2d-nodes mesh)
-  (slot-ref mesh 'nodes))
-
-(define (mesh2d-triangles mesh)
-  (slot-ref mesh 'triangles))
-
-(define (mesh2d-nodes-length mesh)
-  (vview-length (mesh2d-nodes mesh) 0))
-
-(define (mesh2d-nodes-ref mesh i)
-  (vview-cut (mesh2d-nodes mesh) (vector i)))
-
-(define (mesh2d-triangles-ref mesh i)
-  (vview-cut (mesh2d-triangles mesh) (vector i)))
-
-(define (square nx ny)
-  (define ns (square-point-vec (vector-linspace 0. 1. nx)
-                               (vector-linspace 0. 1. ny)))
-  (define ts (square-triangle-vec nx ny))
-  (make-mesh2d
-   (make-vview ns 0 (vector (* nx ny) 2))
-   (make-vview ts 0 (vector (* 2 (- nx 1) (- ny 1)) 3))))
-
 
 ;; cafemielk.fel
 
---
+++ cm-tight/cafemielk/mesh.scm
@@ -0,0 +1,54 @@
+;;;
+;;; cafemielk.mesh
+;;; Mesh library
+;;;
+
+(define-module cafemielk.mesh
+  (export
+   <mesh2d>
+   make-mesh2d
+   mesh2d-nodes
+   mesh2d-nodes-length
+   mesh2d-nodes-ref
+   mesh2d-triangles
+   mesh2d-triangles-ref
+   square
+   )
+  )
+
+(select-module cafemielk.mesh)
+
+(dynamic-load "cafemielk")
+(use cafemielk.util)
+(use cafemielk.vview)
+
+(define-class <mesh2d> ()
+  ((nodes :init-keyword :nodes)
+   (triangles :init-keyword :triangles)))
+
+(define (make-mesh2d nodes triangles)
+  (make <mesh2d> :nodes nodes :triangles triangles))
+
+(define (mesh2d-nodes mesh)
+  (slot-ref mesh 'nodes))
+
+(define (mesh2d-triangles mesh)
+  (slot-ref mesh 'triangles))
+
+(define (mesh2d-nodes-length mesh)
+  (vview-length (mesh2d-nodes mesh) 0))
+
+(define (mesh2d-nodes-ref mesh i)
+  (vview-cut (mesh2d-nodes mesh) (vector i)))
+
+(define (mesh2d-triangles-ref mesh i)
+  (vview-cut (mesh2d-triangles mesh) (vector i)))
+
+(define (square nx ny)
+  (define ns (square-point-vec (vector-linspace 0. 1. nx)
+                               (vector-linspace 0. 1. ny)))
+  (define ts (square-triangle-vec nx ny))
+  (make-mesh2d
+   (make-vview ns 0 (vector (* nx ny) 2))
+   (make-vview ts 0 (vector (* 2 (- nx 1) (- ny 1)) 3))))
+
