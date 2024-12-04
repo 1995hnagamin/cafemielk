c24f31af2d7ba8878e5fed8a8049cd8d012cc4d6
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-12-05 01:36:28 +0900

    Use incf instead of setf

--- clcm/cafemielk/linalg.lisp
+++ clcm/cafemielk/linalg.lisp
@@ -23,19 +23,19 @@
 
 (defun vector-addv! (y x)
   "y += x"
-  (loop for i below (length y)
-        do (setf (aref y i) (+ (aref y i) (aref x i)))))
+  (loop for i below (length y) do
+        (incf (aref y i) (aref x i))))
 
 (defun vector-rescale-addv! (y c x)
   "y := c * y + x"
-  (loop for i below (length y)
-        do (setf (aref y i)
-                 (+ (* (aref y i) c) (aref x i)))))
+  (loop for i below (length y) do
+        (setf (aref y i)
+              (+ (* (aref y i) c) (aref x i)))))
 
 (defun vector-addcv! (y c x)
   "y += c * x"
-  (loop for i below (length y)
-        do (setf (aref y i) (+ (aref y i) (* (aref x i) c)))))
+  (loop for i below (length y) do
+        (incf (aref y i) (* (aref x i) c))))
 
 (defstruct dense-matrix
   (data #2a() :type (array number (* *))))
@@ -52,9 +52,8 @@
   "y += M * x"
   (loop for i below nrow do
         (loop for j below ncol do
-              (setf (aref y i)
-                    (+ (aref y i)
-                       (* (matrix-ref M i j) (aref x j)))))))
+              (incf (aref y i)
+                    (* (matrix-ref M i j) (aref x j))))))
 
 
 
