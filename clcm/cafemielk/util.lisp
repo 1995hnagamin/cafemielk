cf222d0497e497a61b6ff2ffed595391ce46f4d4
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-12-28 23:37:33 +0900

    Add more utilities from util.scm to util.lisp

--- clcm/cafemielk/util.lisp
+++ clcm/cafemielk/util.lisp
@@ -10,6 +10,17 @@
   `(let ((,var ,init))
      ,@body))
 
+(defun linspace (min max size)
+  (let1 step (/ (- max min) (1- size))
+    (loop :for i :from 0 :below size
+          :collecting (+ min (* step i)))))
+
+(defun vector-linspace (min max size)
+  (let ((vec (make-array size))
+        (step (/ (- max min) (1- size))))
+    (loop :for i :from 0 :below size
+          :do (setf (aref vec i) (+ min (* step i))))
+    vec))
 
 (defmacro collect-substitute (collect (params &rest args-list) expr)
   `(,collect
@@ -26,5 +37,23 @@
        ((,i ,j ,k) (0 1 2) (1 2 0) (2 0 1))
        ,expr))
 
+(defmacro every-substitute-a3 ((i j k) expr)
+  `(collect-substitute-a3 and (,i ,j ,k) ,expr))
+
 (defmacro vec3d-tab ((i j k) expr)
   `(collect-substitute-a3 vector (,i ,j ,k) ,expr))
+
+(defun cross2d (u v)
+  (- (* (aref u 0) (aref v 1))
+     (* (aref u 1) (aref v 0))))
+
+(defun cross3d (u v)
+  (vec3d-tab
+      (i i+1 i+2)
+      (- (* (aref u i+1) (aref v i+2))
+         (* (aref u i+2) (aref v i+1)))))
+
+(defun dot-product (u v)
+  (loop :for i :from 0 :below (array-dimension u 0)
+        :sum (* (aref u i) (aref v i))))
+
