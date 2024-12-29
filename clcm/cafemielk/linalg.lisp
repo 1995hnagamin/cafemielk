1d831b5df06720c28272fe6ab56c2ce1a0e86f40
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-12-30 00:03:38 +0900

    Add CSR utilities to linalg.lisp

--- clcm/cafemielk/linalg.lisp
+++ clcm/cafemielk/linalg.lisp
@@ -55,7 +55,50 @@
               (incf (aref y i)
                     (* (matrix-ref M i j) (aref x j))))))
 
+;;;
+;;; CSR (compressed sparse row)
+;;;
 
+
+(defstruct csr
+  (entries)
+  (rowptr #() :type (array fixnum (*)))
+  (colind #() :type (array fixnum (*))))
+
+(defun csr-addmv! (y nrow ncol A x)
+  (declare (ignore ncol))
+  (with-slots (entries rowptr colind) A
+    (loop :for i :from 0 :below nrow :do
+      (loop :for j :from (aref rowptr i) :below (aref rowptr (1+ i)) :do
+        (incf (aref y i)
+              (* (aref entries j)
+                 (aref x (aref colind j))))))))
+
+(defun csr-mv-set! (y nrow ncol A x)
+  (loop :for i :from 0 :below nrow :do
+    (setf (aref y i) 0))
+  (csr-addmv! y nrow ncol A x))
+
+(defun csr-mv (nrow ncol A x)
+  (let1 y (make-array nrow :initial-element 0)
+    (csr-addmv! y nrow ncol A x)
+    y))
+
+(defmethod mv (nrow ncol (A csr) v)
+  (csr-mv nrow ncol A v))
+
+(defmethod mv-set! (y nrow ncol (A csr) v)
+  (csr-mv-set! y nrow ncol A v))
+
+(defun csr-ref (A i j)
+  (with-slots (entries rowptr colind) A
+    (loop :for idx :from (aref rowptr i) :below (aref rowptr (1+ i))
+            :thereis (and (= j (aref colind idx))
+                          (aref entries idx))
+          :finally (return 0))))
+
+(defmethod matrix-ref ((M csr) i j)
+  (csr-ref M i j))
 
 ;;; Local Variables:
 ;;; mode: lisp
