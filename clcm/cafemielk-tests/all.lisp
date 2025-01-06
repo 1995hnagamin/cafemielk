8bb416cbbf2538ac42848a8b873e3fd6dacfb52c
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2025-01-06 18:12:11 +0900

    Use package-local nickname :cm in test file

--- clcm/cafemielk-tests/all.lisp
+++ clcm/cafemielk-tests/all.lisp
@@ -1,17 +1,18 @@
 (defpackage :cafemielk-tests/all
-  (:use :cl :cafemielk)
+  (:use :cl)
+  (:local-nicknames (:cm :cafemielk))
   (:export :run-tests))
 
 (in-package :cafemielk-tests/all)
 
 ;; WolframAlpha: `cross ((3,141,5),(92,65,35))`
 (defun test-cross3d ()
-  (equalp (cafemielk:cross3d #(3 141 5) #(92 65 35))
+  (equalp (cm:cross3d #(3 141 5) #(92 65 35))
           #(4610 355 -12777)))
 
 ;; WolframAlpha: `triangle (3,653) (141,589) (592,793)`
 (defun test-trig2d-area ()
-  (= (cafemielk:trig2d-area #(3 141 592 653 589 793))
+  (= (cm:trig2d-area #(3 141 592 653 589 793))
      28508))
 
 ;; / 1  2  0  0  0 \ / 5 \ = / 13 \
@@ -20,14 +21,14 @@
 ;; | 0  0  9 10 11 | | 2 |   | 58 |
 ;; \ 0  0  0 12 13 / \ 1 /   \ 37 /
 (defun test-csr-mv ()
-  (let ((A (cafemielk:coo->csr
-            (cafemielk:make-coo
+  (let ((A (cm:coo->csr
+            (cm:make-coo
              :nrow 5 :ncol 5
              :entries #(1 2 3 4 5 6 7 8 9 10 11 12 13)
              :rowind #(0 0 1 1 1 2 2 2 3 3 3 4 4)
              :colind #(0 1 0 1 2 1 2 3 2 3 4 3 4))))
         (x #(5 4 3 2 1)))
-    (equalp (cafemielk:mv A x) #(13 46 61 58 37))))
+    (equalp (cm:mv A x) #(13 46 61 58 37))))
 
 (defmacro report-tests (&rest test-exprs)
   `(progn
