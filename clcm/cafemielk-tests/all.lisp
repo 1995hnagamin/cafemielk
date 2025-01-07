32b5bcc2966f5c0c3da90475562312267c1b9225
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2025-01-08 00:05:38 +0900

    Add test-cases for rvd->csr and rvd-row-count

--- clcm/cafemielk-tests/all.lisp
+++ clcm/cafemielk-tests/all.lisp
@@ -19,12 +19,25 @@
          28508)))
 
 (test test-rvd
-  (let ((A (cm:create-empty-rvd 10 10 :element-type 'double-float)))
+  (let ((target (cm:make-csr
+                 :nrow 10 :ncol 10
+                 :rowptr (coerce #(0 0 1 3 4 4 4 4 4 4 4)
+                                 '(simple-array fixnum (11)))
+                 :colind (coerce #(0 1 9 3)
+                                 '(simple-array fixnum (4)))
+                 :entries (coerce #(50.0d0 21.0d0 29.0d0 33.0d0)
+                                  '(simple-array double-float (4)))))
+        (A (cm:create-empty-rvd 10 10 :element-type 'double-float)))
     (setf (cm:get-rvd A 3 3) 33.0d0)
+    (incf (cm:get-rvd A 2 1) 21.0d0)
+    (incf (cm:get-rvd A 2 9) 29.0d0)
     (incf (cm:get-rvd A 1 0) 10.0d0)
     (incf (cm:get-rvd A 1 0) 40.0d0)
     (is (= 33.0d0 (cm:get-rvd A 3 3)))
-    (is (= 50.0d0 (cm:get-rvd A 1 0)))))
+    (is (= 50.0d0 (cm:get-rvd A 1 0)))
+    (is (= 2 (cm:rvd-row-count A 2)))
+    (is (equalp target
+                (cm:rvd->csr A :element-type 'double-float)))))
 
 ;; / 1  2  0  0  0 \ / 5 \ = / 13 \
 ;; | 3  4  5  0  0 | | 4 |   | 46 |
