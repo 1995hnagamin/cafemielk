16fd48e8f4eb74acc54a7e15b6efcf498f0acc60
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2025-02-03 23:49:07 +0900

    Add helper functions to delaunay.lisp

--- clcm/cafemielk/mesh2d/delaunay.lisp
+++ clcm/cafemielk/mesh2d/delaunay.lisp
@@ -6,6 +6,22 @@
    ))
 (in-package :cafemielk/mesh2d/delaunay)
 
+(defun square (nx ny &key (element-type t))
+  (loop
+    :with array := (make-array `(,(* (1+ nx) (1+ ny)) 2)
+                               :element-type element-type)
+    :for j :from 0 :to ny
+    :do
+       (loop
+         :for i :from 0 :to nx
+         :do
+            (setf (aref array (+ (* (1+ nx) j) i) 0)
+                  (coerce (/ i nx) element-type))
+            (setf (aref array (+ (* (1+ nx) j) i) 1)
+                  (coerce (/ j ny) element-type)))
+    :finally
+       (return array)))
+
 (defun random-range (start end)
   (+ start (random (- end start))))
 
@@ -17,3 +33,36 @@
        (rotatef (aref array i) (aref array j))
     :finally
        (return array)))
+
+(declaim (inline iota-array))
+(defun iota-array (count &key (start 0) (step 1) (element-type t))
+  (loop
+    :with array := (make-array `(,count)
+                               :element-type element-type)
+    :for i :from 0 :below count
+    :do
+       (setf (aref array i) (+ start (* i step)))
+    :finally
+       (return array)))
+
+(defun copy-vector-adjustable (vector)
+  (let ((length (length vector)))
+    (make-array `(,length)
+                :initial-contents vector
+                :adjustable t
+                :fill-pointer length)))
+
+(defun delete-nth (n vector)
+  (loop
+    :for i :from (1+ n) :below (length vector)
+    :do
+       (rotatef (aref vector (1- i)) (aref vector i))
+    :finally
+       (vector-pop vector)))
+
+(declaim (inline <=>))
+(defun <=> (a b)
+  (cond
+    ((< a b) :<)
+    ((> a b) :>)
+    (t :=)))
