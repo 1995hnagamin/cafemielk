c6fe5a92d92f96348c19f32f133398173ad21507
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2025-02-10 21:42:54 +0900

    Tidy Separate virtual point case in delaunay-ccw-p

--- clcm/cafemielk/mesh2d/delaunay.lisp
+++ clcm/cafemielk/mesh2d/delaunay.lisp
@@ -149,14 +149,18 @@
 
 (defun delaunay-ccw-p (r i j &key point-array)
   (declare (type fixnum r i j))
-  (cond
-    ((and (>= r 0) (>= i 0) (>= j 0))
-     (counterclockwisep (point-array-nth r point-array)
-                        (point-array-nth i point-array)
-                        (point-array-nth j point-array)))
-    ((= i -2) (delaunay-lex< j r :point-array point-array))
-    ((= j -1) (delaunay-lex< i r :point-array point-array))
-    (t (delaunay-ccw-p i j r :point-array point-array))))
+  (labels ((ccwp (r i j)
+             (cond
+               ((= i -2) (delaunay-lex< j r :point-array point-array))
+               ((= j -1) (delaunay-lex< i r :point-array point-array))
+               (t (ccwp i j r)))))
+    (if (and (>= r 0) (>= i 0) (>= j 0))
+        ;; Normal case
+        (counterclockwisep (point-array-nth r point-array)
+                           (point-array-nth i point-array)
+                           (point-array-nth j point-array))
+        ;; Parameters involve at least one virtual point
+        (ccwp r i j))))
 
 (defun delaunay-inner-p (r trig-vise point-array)
   (aref-let1 (i j k) trig-vise
