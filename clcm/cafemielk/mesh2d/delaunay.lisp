0cf29163f263f12ddeb62e42d9ccbc63ad3f5e64
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2025-02-15 19:42:52 +0900

    Remove duplicate definitions on point-arrays

--- clcm/cafemielk/mesh2d/delaunay.lisp
+++ clcm/cafemielk/mesh2d/delaunay.lisp
@@ -67,34 +67,6 @@
     :finally
        (vector-pop vector)))
 
-(declaim (inline point-array-xref))
-(defun point-array-xref (point-array i)
-  (declare (type fixnum i)
-           (type (simple-array * (* 2)) point-array))
-  (aref point-array i 0))
-
-(declaim (inline point-array-yref))
-(defun point-array-yref (point-array i)
-  (declare (type fixnum i)
-           (type (simple-array * (* 2)) point-array))
-  (aref point-array i 1))
-
-(defun vise->trig (point-array vise)
-  `#(,(point-array-xref point-array (aref vise 0))
-     ,(point-array-xref point-array (aref vise 1))
-     ,(point-array-xref point-array (aref vise 2))
-     ,(point-array-yref point-array (aref vise 0))
-     ,(point-array-yref point-array (aref vise 1))
-     ,(point-array-yref point-array (aref vise 2))))
-
-
-(declaim (inline point-array-nth))
-(defun point-array-nth (n point-array)
-  (declare (type fixnum n)
-           (type (simple-array * (* 2)) point-array))
-  `#(,(point-array-xref point-array n)
-     ,(point-array-yref point-array n)))
-
 (defmacro lexicographic< ((i j) (param) &rest clauses)
   (once-only (i j)
     (reduce
